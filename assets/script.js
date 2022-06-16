3628a64d1f581a054bc37cd0608287949d60b6df
geraldo.tech <geraldo.filho92@hotmail.com>
2022-06-16 19:24:40 -0300

    add get avatar from github api

--- assets/script.js
+++ assets/script.js
@@ -9,3 +9,25 @@ function typeWriter(el){
 let a = '<h1>COSTA</h1>'
 const title = document.querySelector("#write");
 typeWriter(title)
+
+
+
+//getting avatar_url frrom GitHub
+const api = "https://api.github.com/users/geraldotech";
+
+async function getimg(url){
+    const response = await fetch(url);
+    const data = await response.json();
+
+    console.log(data);
+    if(response){
+        show(data)
+    }
+}
+
+getimg(api);
+
+function show(data){
+    document.getElementById("myavatar").src = `${data.avatar_url}`;
+    console.log( `${data.avatar_url}`)
+}
